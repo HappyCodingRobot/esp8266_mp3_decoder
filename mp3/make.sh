e9e8a9274911d9145efd99c8bd5fb6fce4e6abc8
HappyCodingRobot <HappyCodingRobot@users.noreply.github.com>
2015-12-21 12:57:51 +0100

    testing different flash configurations

--- mp3/make.sh
+++ mp3/make.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
-make COMPILE=gcc BOOT=none APP=0 SPI_SPEED=40 SPI_MODE=QIO SPI_SIZE=1024
-${ESPTOOL} --port ${ESPPORT} --baud ${ESPBAUD} write_flash 0x00000 ../bin/eagle.flash.bin 0xA0000 ../bin/eagle.irom0text.bin
+ESPTOOL=esptool.py
+ESPPORT=/dev/ttyUSB0
+ESPBAUD=115200
+## 512k settings
+#make COMPILE=gcc BOOT=none APP=0 SPI_SPEED=40 SPI_MODE=QIO SPI_SIZE=512
+#${ESPTOOL} --port ${ESPPORT} --baud ${ESPBAUD} write_flash 0x00000 ../bin/eagle.flash.bin 0xA0000 ../bin/eagle.irom0text.bin
 
+## 1M & 4M settings
+# make COMPILE=gcc BOOT=none APP=0 SPI_SPEED=40 SPI_MODE=QIO SPI_SIZE=1024
+make COMPILE=gcc BOOT=none APP=0 SPI_SPEED=40 SPI_MODE=QIO SPI_SIZE=4096
+${ESPTOOL} --port ${ESPPORT} --baud ${ESPBAUD} write_flash 0x00000 ../bin/eagle.flash.bin 0xA0000 ../bin/eagle.irom0text.bin
